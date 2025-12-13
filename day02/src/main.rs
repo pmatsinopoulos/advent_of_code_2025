48420dcbdb1d5b366273a4c4cc8efa504a6b72fb
Panos Matsinopoulos <panagiotis@matsinopoulos.gr>
2025-12-13 15:04:54 +0200

    Range from string and Ranges from string

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -1,4 +1,5 @@
 use std::cmp::PartialEq;
+use std::ops::Range;
 
 fn main() {
     println!("Hello, world!");
@@ -37,6 +38,26 @@ fn validate(input: &str) -> ValidationResult {
     }
     ValidationResult::Valid
 }
+
+fn range_from_string(input: &str) -> Range<u64> {
+    let (left, right) = input.split_once("-").unwrap();
+    let start: u64 = left.parse().unwrap();
+    let end: u64 = right.parse().unwrap();
+    Range {
+        start,
+        end: end + 1,
+    }
+}
+
+fn ranges_from_string(input: &str) -> Vec<Range<u64>> {
+    let ranges_strings: Vec<&str> = input.split(',').collect();
+    ranges_strings
+        .iter()
+        .map(|x| range_from_string(x))
+        .collect()
+}
+
+// ----- validate() -----------------------------------------------------------------------
 
 #[test]
 fn test_case_1() {
@@ -106,4 +127,39 @@ fn test_case_10() {
     let input = "64164";
     let validation_result = validate(input);
     assert_eq!(validation_result, ValidationResult::Valid);
+}
+
+// --- range_from_string -------------------------
+
+#[test]
+fn test_range_from_string_case_1() {
+    let input = "11-25";
+    let range_result = range_from_string(input);
+    assert_eq!(range_result, Range { start: 11, end: 26 });
+}
+
+// --- ranges_from_string ---------------------
+
+#[test]
+fn test_ranges_from_string_case_1() {
+    let input = "11-22,95-115,998-1012,1188511880-1188511890";
+    let ranges = ranges_from_string(input);
+    assert_eq!(
+        ranges,
+        vec![
+            Range { start: 11, end: 23 },
+            Range {
+                start: 95,
+                end: 116
+            },
+            Range {
+                start: 998,
+                end: 1013
+            },
+            Range {
+                start: 1188511880,
+                end: 1188511891,
+            }
+        ]
+    )
 }
