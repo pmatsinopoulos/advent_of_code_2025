21de2f2ee8b510bbbefb6eb23a02906426b22a0a
Panos Matsinopoulos <panagiotis@matsinopoulos.gr>
2025-12-30 14:49:55 +0200

    In a vector of ranges I reduce their number by merging overlapping ranges

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -31,11 +31,68 @@ fn compare_ranges(
     if range_lhs.start() < range_rhs.start() {
         Ordering::Less
     } else if range_lhs.start() == range_rhs.start() {
-        Ordering::Equal
+        if range_lhs.end() < range_rhs.end() {
+            Ordering::Less
+        } else if range_lhs.end() == range_rhs.end() {
+            Ordering::Equal
+        } else {
+            Ordering::Greater
+        }
     } else {
         Ordering::Greater
     }
 }
+
+// I have the way to sort ranges. But these ranges might be overlapping.
+// I can merge the overlapping and end having less ranges.
+//
+// Merging two ranges when they are overlapping
+
+fn merge_overlapping_ranges(
+    range1: &RangeInclusive<RangeBoundary>,
+    range2: &RangeInclusive<RangeBoundary>,
+) -> Result<RangeInclusive<RangeBoundary>, String> {
+    if *range1.end() < *range2.start() - 1 || *range2.end() < *range1.start() - 1 {
+        return Err("Non overlapping ranges".to_string());
+    }
+    if *range1.end() == *range2.start() - 1 {
+        return Ok(*range1.start()..=*range2.end());
+    }
+    if *range2.end() == *range1.start() - 1 {
+        return Ok(*range2.start()..=*range1.end());
+    }
+    Ok(min(*range1.start(), *range2.start())..=max(*range1.end(), *range2.end()))
+}
+
+// Remove overlapping ranges by merging.
+// Note that this assumes that ranges are sorted by their `.start()`.
+
+fn remove_overlapping_ranges(
+    ranges: &Vec<RangeInclusive<RangeBoundary>>,
+) -> Vec<RangeInclusive<RangeBoundary>> {
+    let mut non_overlapping_ranges: Vec<RangeInclusive<RangeBoundary>> = vec![];
+    for (i, range) in ranges.iter().enumerate() {
+        if i == 0 {
+            non_overlapping_ranges.push(range.clone());
+            continue;
+        }
+        let merge_result = merge_overlapping_ranges(
+            &non_overlapping_ranges[non_overlapping_ranges.len() - 1],
+            &range,
+        );
+        if merge_result.is_ok() {
+            non_overlapping_ranges.pop();
+            non_overlapping_ranges.push(merge_result.unwrap());
+        } else {
+            non_overlapping_ranges.push(range.clone());
+        }
+    }
+    non_overlapping_ranges
+}
+
+// ------------------------------------------------------------------
+// Unit Tests
+// ------------------------------------------------------------------
 
 #[test]
 fn test_turn_into_range_case_1() {
@@ -90,25 +147,19 @@ fn test_compare_ranges_case_2() {
 
 #[test]
 fn test_compare_ranges_case_3() {
+    let mut ranges = vec![3..=5, 3..=10];
+    ranges.sort_by(|lhs, rhs| compare_ranges(lhs, rhs));
+    assert_eq!(ranges, vec![3..=5, 3..=10]);
+}
+
+#[test]
+fn test_compare_ranges_case_4() {
     let mut ranges = vec![3..=10, 3..=5];
     ranges.sort_by(|lhs, rhs| compare_ranges(lhs, rhs));
-    assert_eq!(ranges, vec![3..=10, 3..=5]);
+    assert_eq!(ranges, vec![3..=5, 3..=10]);
 }
 
-// I have the way to sort ranges. But these ranges might be overlapping.
-// I can merge the overlapping and end having less ranges.
-//
-// Merging two ranges when they are overlapping
-
-fn merge_overlapping_ranges(
-    range1: &RangeInclusive<RangeBoundary>,
-    range2: &RangeInclusive<RangeBoundary>,
-) -> Result<RangeInclusive<RangeBoundary>, String> {
-    if range1.end() < range2.start() || range2.end() < range1.start() {
-        return Err("Non overlapping ranges".to_string());
-    }
-    Ok(min(*range1.start(), *range2.start())..=max(*range1.end(), *range2.end()))
-}
+// merge_overlapping_ranges
 
 #[test]
 fn test_merge_overlapping_ranges_case_1() {
@@ -123,10 +174,7 @@ fn test_merge_overlapping_ranges_case_2() {
     let range1 = 1..=5;
     let range2 = 6..=8;
     let non_overlapping_range = merge_overlapping_ranges(&range1, &range2);
-    assert_eq!(
-        non_overlapping_range,
-        Err("Non overlapping ranges".to_string())
-    );
+    assert_eq!(non_overlapping_range, Ok(1..=8));
 }
 
 #[test]
@@ -134,10 +182,7 @@ fn test_merge_overlapping_ranges_case_3() {
     let range1 = 6..=8;
     let range2 = 1..=5;
     let non_overlapping_range = merge_overlapping_ranges(&range1, &range2);
-    assert_eq!(
-        non_overlapping_range,
-        Err("Non overlapping ranges".to_string())
-    );
+    assert_eq!(non_overlapping_range, Ok(1..=8));
 }
 
 #[test]
@@ -146,4 +191,74 @@ fn test_merge_overlapping_ranges_case_4() {
     let range2 = 1..=5;
     let non_overlapping_range = merge_overlapping_ranges(&range1, &range2);
     assert_eq!(non_overlapping_range, Ok(1..=6));
+}
+
+#[test]
+fn test_merge_overlapping_ranges_case_5() {
+    let range1 = 1..=5;
+    let range2 = 5..=8;
+    let non_overlapping_range = merge_overlapping_ranges(&range1, &range2);
+    assert_eq!(non_overlapping_range, Ok(1..=8));
+}
+
+#[test]
+fn test_merge_overlapping_ranges_case_6() {
+    let range1 = 1..=5;
+    let range2 = 6..=8;
+    let non_overlapping_range = merge_overlapping_ranges(&range1, &range2);
+    assert_eq!(non_overlapping_range, Ok(1..=8));
+}
+
+// remove_overlapping_ranges
+
+#[test]
+fn test_remove_overlapping_ranges_case_1() {
+    let ranges: Vec<RangeInclusive<RangeBoundary>> = vec![1..=5, 2..=6, 8..=20];
+    let non_overlapping_ranges = remove_overlapping_ranges(&ranges);
+    assert_eq!(non_overlapping_ranges, vec![1..=6, 8..=20]);
+}
+
+#[test]
+fn test_remove_overlapping_ranges_case_2() {
+    let ranges: Vec<RangeInclusive<RangeBoundary>> = vec![1..=5, 8..=20];
+    let non_overlapping_ranges = remove_overlapping_ranges(&ranges);
+    assert_eq!(non_overlapping_ranges, vec![1..=5, 8..=20]);
+}
+
+#[test]
+fn test_remove_overlapping_ranges_case_3() {
+    let ranges: Vec<RangeInclusive<RangeBoundary>> = vec![1..=5];
+    let non_overlapping_ranges = remove_overlapping_ranges(&ranges);
+    assert_eq!(non_overlapping_ranges, vec![1..=5]);
+}
+
+#[test]
+fn test_remove_overlapping_ranges_case_4() {
+    let ranges: Vec<RangeInclusive<RangeBoundary>> = vec![];
+    let non_overlapping_ranges = remove_overlapping_ranges(&ranges);
+    assert_eq!(non_overlapping_ranges, vec![]);
+}
+
+#[test]
+fn test_remove_overlapping_ranges_case_5() {
+    let ranges: Vec<RangeInclusive<RangeBoundary>> = vec![1..=5, 5..=8, 5..=9, 8..=10];
+    let non_overlapping_ranges = remove_overlapping_ranges(&ranges);
+    assert_eq!(non_overlapping_ranges, vec![1..=10]);
+}
+
+#[test]
+fn test_remove_overlapping_ranges_case_6() {
+    let ranges: Vec<RangeInclusive<RangeBoundary>> = vec![1..=5, 5..=7, 5..=8, 9..=11];
+    let non_overlapping_ranges = remove_overlapping_ranges(&ranges);
+    assert_eq!(non_overlapping_ranges, vec![1..=11]);
+}
+
+// sort and remove
+
+#[test]
+fn test_sort_and_remove_overlapping_ranges_case_1() {
+    let mut ranges: Vec<RangeInclusive<RangeBoundary>> = vec![5..=7, 1..=5, 9..=11, 5..=8];
+    ranges.sort_by(|lhs, rhs| compare_ranges(lhs, rhs));
+    let non_overlapping_ranges = remove_overlapping_ranges(&ranges);
+    assert_eq!(non_overlapping_ranges, vec![1..=11]);
 }
