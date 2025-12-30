0041c6f4e2d7cdf7d939e099d9fdc24ef536b1ad
Panos Matsinopoulos <panagiotis@matsinopoulos.gr>
2025-12-30 13:01:50 +0200

    I can sort the ranges by their start. This will be useful when trying to test whether a product is fresh or not

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -1,22 +1,39 @@
+use std::cmp::Ordering;
 use std::ops::RangeInclusive;
 
 fn main() {
     println!("Hello, world!");
 }
 
-fn turn_into_range(line: &str) -> Option<RangeInclusive<u32>> {
+type RangeBoundary = u64;
+type IngredientsRange = RangeInclusive<RangeBoundary>;
+
+fn turn_into_range(line: &str) -> Option<IngredientsRange> {
     if line.is_empty() || line.len() < 3 {
         return None;
     }
 
-    let boundaries: Vec<u32> = line
+    let boundaries: Vec<RangeBoundary> = line
         .split("-")
         .into_iter()
-        .map(|i| i.parse::<u32>().unwrap_or_default())
+        .map(|i| i.parse::<RangeBoundary>().unwrap_or_default())
         .take(2)
         .collect();
 
     Some(boundaries[0]..=boundaries[1])
+}
+
+fn compare_ranges(
+    range_lhs: &RangeInclusive<RangeBoundary>,
+    range_rhs: &RangeInclusive<RangeBoundary>,
+) -> Ordering {
+    if range_lhs.start() < range_rhs.start() {
+        Ordering::Less
+    } else if range_lhs.start() == range_rhs.start() {
+        Ordering::Equal
+    } else {
+        Ordering::Greater
+    }
 }
 
 #[test]
@@ -52,4 +69,27 @@ fn test_turn_into_range_case_5() {
     let line = "1-";
     let result = turn_into_range(line);
     assert_eq!(result, None);
+}
+
+// Compare Ranges
+
+#[test]
+fn test_compare_ranges_case_1() {
+    let mut ranges = vec![3..=10, 2..=5];
+    ranges.sort_by(|lhs, rhs| compare_ranges(lhs, rhs));
+    assert_eq!(ranges, vec![2..=5, 3..=10]);
+}
+
+#[test]
+fn test_compare_ranges_case_2() {
+    let mut ranges = vec![2..=10, 3..=5];
+    ranges.sort_by(|lhs, rhs| compare_ranges(lhs, rhs));
+    assert_eq!(ranges, vec![2..=10, 3..=5]);
+}
+
+#[test]
+fn test_compare_ranges_case_3() {
+    let mut ranges = vec![3..=10, 3..=5];
+    ranges.sort_by(|lhs, rhs| compare_ranges(lhs, rhs));
+    assert_eq!(ranges, vec![3..=10, 3..=5]);
 }
