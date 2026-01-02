f0c02769628248c690489da2585a86438557e654
Panos Matsinopoulos <panagiotis@matsinopoulos.gr>
2026-01-02 09:35:04 +0200

    Use a transpose to transpose rows/colums of the original vector

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -10,49 +10,59 @@ fn main() {
 
 type Number = u64;
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 enum Operator {
     Addition,
     Multiplication,
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 enum Item {
     NumberItem(Number),
     OperatorItem(Operator),
 }
 
-fn grand_total(lines: &str) -> Number {
-    let vectors = to_vec_of_vecs(lines);
-    let mut column = 0;
-    let mut row;
-    let number_of_columns = vectors[0].len();
-    let number_of_rows = vectors.len();
-    let mut result = 0;
-    while column < number_of_columns {
-        let operator = &vectors[number_of_rows - 1][column];
-        let mut column_result = match operator {
-            Item::OperatorItem(Operator::Addition) => 0,
-            Item::OperatorItem(Operator::Multiplication) => 1,
-            _ => panic!("Wrong data. Operator can't be determined"),
-        };
-        row = 0;
-        while row < number_of_rows - 1 {
-            match operator {
-                Item::OperatorItem(Operator::Addition) => match vectors[row][column] {
-                    Item::NumberItem(number) => column_result += number,
-                    _ => (),
-                },
-                Item::OperatorItem(Operator::Multiplication) => match vectors[row][column] {
-                    Item::NumberItem(number) => column_result *= number,
-                    _ => (),
-                },
-                _ => (),
-            }
-            row += 1;
+fn transpose(original: &mut Vec<Vec<Item>>) -> Vec<Vec<Item>> {
+    if original.is_empty() || original[0].is_empty() {
+        return vec![];
+    }
+    let mut result: Vec<Vec<Item>> = vec![];
+    let number_of_rows = original.len();
+    let number_of_columns = original[0].len();
+    for j in 0..number_of_columns {
+        result.push(vec![]);
+        for i in 0..number_of_rows {
+            result[j].push(original[i][j].clone());
         }
-        result += column_result;
-        column += 1;
+    }
+    original.clear();
+    result
+}
+
+fn grand_total(lines: &str) -> Number {
+    let mut vectors = to_vec_of_vecs(lines);
+    let transposed = transpose(&mut vectors);
+    let mut result: Number = 0;
+    for row in &transposed {
+        let (init, operator) = match row[row.len() - 1] {
+            Item::OperatorItem(Operator::Addition) => (0, Operator::Addition),
+            Item::OperatorItem(Operator::Multiplication) => (1, Operator::Multiplication),
+            _ => panic!("Invalid operator"),
+        };
+
+        let row_result = row[0..row.len() - 1]
+            .iter()
+            .fold(init, |acc, item| match operator {
+                Operator::Addition => match item {
+                    Item::NumberItem(number) => acc + number,
+                    _ => panic!("Wrong item type"),
+                },
+                Operator::Multiplication => match item {
+                    Item::NumberItem(number) => acc * number,
+                    _ => panic!("Wrong item type"),
+                },
+            });
+        result += row_result;
     }
     result
 }
@@ -162,4 +172,66 @@ fn test_grand_total_case_1() {
                     ";
     let result = grand_total(input);
     assert_eq!(result, 4277556);
+}
+
+// ----------------- transpose -----------
+
+#[test]
+fn test_transpose_case_1() {
+    let mut input = vec![
+        vec![
+            Item::NumberItem(123),
+            Item::NumberItem(328),
+            Item::NumberItem(51),
+            Item::NumberItem(64),
+        ],
+        vec![
+            Item::NumberItem(45),
+            Item::NumberItem(64),
+            Item::NumberItem(387),
+            Item::NumberItem(23),
+        ],
+        vec![
+            Item::NumberItem(6),
+            Item::NumberItem(98),
+            Item::NumberItem(215),
+            Item::NumberItem(314),
+        ],
+        vec![
+            Item::OperatorItem(Operator::Multiplication),
+            Item::OperatorItem(Operator::Addition),
+            Item::OperatorItem(Operator::Multiplication),
+            Item::OperatorItem(Operator::Addition),
+        ],
+    ];
+    let result = transpose(&mut input);
+    assert_eq!(
+        result,
+        vec![
+            vec![
+                Item::NumberItem(123),
+                Item::NumberItem(45),
+                Item::NumberItem(6),
+                Item::OperatorItem(Operator::Multiplication)
+            ],
+            vec![
+                Item::NumberItem(328),
+                Item::NumberItem(64),
+                Item::NumberItem(98),
+                Item::OperatorItem(Operator::Addition)
+            ],
+            vec![
+                Item::NumberItem(51),
+                Item::NumberItem(387),
+                Item::NumberItem(215),
+                Item::OperatorItem(Operator::Multiplication)
+            ],
+            vec![
+                Item::NumberItem(64),
+                Item::NumberItem(23),
+                Item::NumberItem(314),
+                Item::OperatorItem(Operator::Addition)
+            ]
+        ]
+    )
 }
