e3397efc727398b959c627d40f26d8884db8e1d2
Panos Matsinopoulos <panagiotis@matsinopoulos.gr>
2026-01-04 16:33:34 +0200

    Da06 Part 1 without any polishing and help from AI

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -18,143 +18,149 @@ enum Operator {
 
 #[derive(Debug, PartialEq, Clone)]
 enum Item {
-    NumberItem(Number),
+    NumberItem(String),
     OperatorItem(Operator),
 }
 
-fn transpose(original: &mut Vec<Vec<Item>>) -> Vec<Vec<Item>> {
-    if original.is_empty() || original[0].is_empty() {
-        return vec![];
-    }
-    let mut result: Vec<Vec<Item>> = vec![];
-    let number_of_rows = original.len();
-    let number_of_columns = original[0].len();
-    for j in 0..number_of_columns {
-        result.push(vec![]);
-        for i in 0..number_of_rows {
-            result[j].push(original[i][j].clone());
-        }
-    }
-    original.clear();
-    result
-}
-
 fn grand_total(lines: &str) -> Number {
-    let mut vectors = to_vec_of_vecs(lines);
-    let transposed = transpose(&mut vectors);
-    let mut result: Number = 0;
-    for row in &transposed {
-        let (init, operator) = match row[row.len() - 1] {
-            Item::OperatorItem(Operator::Addition) => (0, Operator::Addition),
-            Item::OperatorItem(Operator::Multiplication) => (1, Operator::Multiplication),
-            _ => panic!("Invalid operator"),
-        };
-
-        let row_result = row[0..row.len() - 1]
-            .iter()
-            .fold(init, |acc, item| match operator {
-                Operator::Addition => match item {
-                    Item::NumberItem(number) => acc + number,
-                    _ => panic!("Wrong item type"),
-                },
-                Operator::Multiplication => match item {
-                    Item::NumberItem(number) => acc * number,
-                    _ => panic!("Wrong item type"),
-                },
-            });
-        result += row_result;
-    }
-    result
+    let vectors = to_vec_of_vecs(lines);
+    vectors
+        .iter()
+        .fold(0, |acc, item| acc + solve_problem(item))
 }
 
 fn to_vec_of_vecs(lines: &str) -> Vec<Vec<Item>> {
-    lines.lines().map(to_vec).collect()
-}
+    let vector_of_lines = lines.lines().collect::<Vec<&str>>();
+    if vector_of_lines.is_empty() {
+        return vec![vec![]];
+    }
+    let length_of_first_line = vector_of_lines[0].len();
 
-fn to_vec(line: &str) -> Vec<Item> {
-    if line.is_empty() {
-        return vec![];
+    assert!(
+        vector_of_lines
+            .iter()
+            .filter(|line| !line.is_empty())
+            .all(|line| line.len() == length_of_first_line),
+    );
+
+    // we need to find the first column that it has all values a blank space
+    let mut starting_column = 0;
+    let mut result = vec![];
+    let mut result_item = 0;
+    while starting_column < length_of_first_line {
+        let mut column: usize = starting_column;
+        let mut column_found = false;
+        while column < length_of_first_line {
+            let mut all_spaces = true;
+            let mut row = 0;
+            while row < vector_of_lines.len() {
+                if vector_of_lines[row].chars().nth(column).unwrap() != ' ' {
+                    all_spaces = false;
+                    break;
+                }
+                row += 1;
+            }
+            if row == vector_of_lines.len() && all_spaces {
+                column_found = true;
+                break;
+            }
+            column += 1;
+        }
+
+        if !column_found && starting_column < length_of_first_line {
+            column_found = true;
+            column = length_of_first_line;
+        }
+
+        if column_found {
+            result.push(vec![]);
+            for row in &vector_of_lines {
+                let string = row[starting_column..column].to_string();
+                let first_char = string.chars().next().unwrap();
+                if first_char == '*' {
+                    result[result_item].push(Item::OperatorItem(Operator::Multiplication))
+                } else if first_char == '+' {
+                    result[result_item].push(Item::OperatorItem(Operator::Addition))
+                } else {
+                    result[result_item].push(Item::NumberItem(string))
+                }
+            }
+            result_item += 1;
+            starting_column = column + 1;
+        }
     }
 
-    line.split_whitespace()
-        .map(|string| {
-            let first = string.chars().next().unwrap();
-            if first == '*' {
-                Item::OperatorItem(Operator::Multiplication)
-            } else if first == '+' {
-                Item::OperatorItem(Operator::Addition)
-            } else {
-                Item::NumberItem(string.parse::<Number>().unwrap())
+    result
+}
+
+fn solve_problem(problem: &Vec<Item>) -> Number {
+    let line_length = match &problem[0] {
+        Item::NumberItem(line_string) => line_string.len(),
+        _ => panic!("First line should be Item::NumberItem(String)"),
+    };
+    let operator = &problem[problem.len() - 1];
+    let mut result: Number = match operator {
+        Item::OperatorItem(Operator::Addition) => 0,
+        Item::OperatorItem(Operator::Multiplication) => 1,
+        _ => panic!("Operator should be Item::OperatorItem"),
+    };
+    for digit in 0..line_length {
+        let mut digit_to_number_string = "".to_string();
+        for row in 0..(problem.len() - 1) {
+            match &problem[row] {
+                Item::NumberItem(line_string) => {
+                    let digit_str = line_string.chars().nth(digit).unwrap();
+                    if digit_str != ' ' {
+                        digit_to_number_string.push_str(&digit_str.to_string());
+                    }
+                }
+                _ => panic!("problem[row] should be Item::NumberItem(String)"),
             }
-        })
-        .collect()
-}
-
-#[test]
-fn test_to_vec_case_1() {
-    let input = "123 328    51 64";
-    let result = to_vec(input);
-    assert_eq!(
-        result,
-        vec![
-            Item::NumberItem(123),
-            Item::NumberItem(328),
-            Item::NumberItem(51),
-            Item::NumberItem(64)
-        ]
-    );
-}
-
-#[test]
-fn test_to_vec_case_2() {
-    let input = "* +   *    +";
-    let result = to_vec(input);
-    assert_eq!(
-        result,
-        vec![
-            Item::OperatorItem(Operator::Multiplication),
-            Item::OperatorItem(Operator::Addition),
-            Item::OperatorItem(Operator::Multiplication),
-            Item::OperatorItem(Operator::Addition)
-        ]
-    );
+        }
+        match operator {
+            Item::OperatorItem(Operator::Addition) => {
+                result += digit_to_number_string.parse::<Number>().unwrap()
+            }
+            Item::OperatorItem(Operator::Multiplication) => {
+                result *= digit_to_number_string.parse::<Number>().unwrap()
+            }
+            _ => panic!("Operator should be Item::OperatorItem"),
+        }
+    }
+    result
 }
 
 // ----------- to_vec_of_vecs()
 
 #[test]
 fn test_to_vec_of_vecs_case_1() {
-    let input = "123 328  51 64\r\n\
-                       45 64  387 23\r\n\
-                       6 98  215 314\r\n\
-                       *   +   *   +\r\n\
-                    ";
+    let input = "123 328  51 64 \r\n 45 64  387 23 \r\n  6 98  215 314\r\n*   +   *   +  \r\n";
     let result = to_vec_of_vecs(input);
     assert_eq!(
         result,
         vec![
             vec![
-                Item::NumberItem(123),
-                Item::NumberItem(328),
-                Item::NumberItem(51),
-                Item::NumberItem(64)
+                Item::NumberItem("123".to_string()),
+                Item::NumberItem(" 45".to_string()),
+                Item::NumberItem("  6".to_string()),
+                Item::OperatorItem(Operator::Multiplication)
             ],
             vec![
-                Item::NumberItem(45),
-                Item::NumberItem(64),
-                Item::NumberItem(387),
-                Item::NumberItem(23)
+                Item::NumberItem("328".to_string()),
+                Item::NumberItem("64 ".to_string()),
+                Item::NumberItem("98 ".to_string()),
+                Item::OperatorItem(Operator::Addition)
             ],
             vec![
-                Item::NumberItem(6),
-                Item::NumberItem(98),
-                Item::NumberItem(215),
-                Item::NumberItem(314)
+                Item::NumberItem(" 51".to_string()),
+                Item::NumberItem("387".to_string()),
+                Item::NumberItem("215".to_string()),
+                Item::OperatorItem(Operator::Multiplication)
             ],
             vec![
-                Item::OperatorItem(Operator::Multiplication),
-                Item::OperatorItem(Operator::Addition),
-                Item::OperatorItem(Operator::Multiplication),
+                Item::NumberItem("64 ".to_string()),
+                Item::NumberItem("23 ".to_string()),
+                Item::NumberItem("314".to_string()),
                 Item::OperatorItem(Operator::Addition)
             ]
         ]
@@ -165,73 +171,57 @@ fn test_to_vec_of_vecs_case_1() {
 
 #[test]
 fn test_grand_total_case_1() {
-    let input = "123 328  51 64\r\n\
-                       45 64  387 23\r\n\
-                       6 98  215 314\r\n\
-                       *   +   *   +\r\n\
-                    ";
+    let input = "123 328  51 64 \r\n 45 64  387 23 \r\n  6 98  215 314\r\n*   +   *   +  \r\n";
     let result = grand_total(input);
-    assert_eq!(result, 4277556);
+    assert_eq!(result, 3263827);
 }
 
-// ----------------- transpose -----------
+// ---- solve_problem
 
 #[test]
-fn test_transpose_case_1() {
-    let mut input = vec![
-        vec![
-            Item::NumberItem(123),
-            Item::NumberItem(328),
-            Item::NumberItem(51),
-            Item::NumberItem(64),
-        ],
-        vec![
-            Item::NumberItem(45),
-            Item::NumberItem(64),
-            Item::NumberItem(387),
-            Item::NumberItem(23),
-        ],
-        vec![
-            Item::NumberItem(6),
-            Item::NumberItem(98),
-            Item::NumberItem(215),
-            Item::NumberItem(314),
-        ],
-        vec![
-            Item::OperatorItem(Operator::Multiplication),
-            Item::OperatorItem(Operator::Addition),
-            Item::OperatorItem(Operator::Multiplication),
-            Item::OperatorItem(Operator::Addition),
-        ],
+fn test_solve_problem_case_1() {
+    let input: Vec<Item> = vec![
+        Item::NumberItem("123".to_string()),
+        Item::NumberItem(" 45".to_string()),
+        Item::NumberItem("  6".to_string()),
+        Item::OperatorItem(Operator::Multiplication),
     ];
-    let result = transpose(&mut input);
-    assert_eq!(
-        result,
-        vec![
-            vec![
-                Item::NumberItem(123),
-                Item::NumberItem(45),
-                Item::NumberItem(6),
-                Item::OperatorItem(Operator::Multiplication)
-            ],
-            vec![
-                Item::NumberItem(328),
-                Item::NumberItem(64),
-                Item::NumberItem(98),
-                Item::OperatorItem(Operator::Addition)
-            ],
-            vec![
-                Item::NumberItem(51),
-                Item::NumberItem(387),
-                Item::NumberItem(215),
-                Item::OperatorItem(Operator::Multiplication)
-            ],
-            vec![
-                Item::NumberItem(64),
-                Item::NumberItem(23),
-                Item::NumberItem(314),
-                Item::OperatorItem(Operator::Addition)
-            ]
-        ]
-    )
+    let result = solve_problem(&input);
+    assert_eq!(result, 8544);
+}
+
+#[test]
+fn test_solve_problem_case_2() {
+    let input: Vec<Item> = vec![
+        Item::NumberItem("328".to_string()),
+        Item::NumberItem("64 ".to_string()),
+        Item::NumberItem("98 ".to_string()),
+        Item::OperatorItem(Operator::Addition),
+    ];
+    let result = solve_problem(&input);
+    assert_eq!(result, 625);
+}
+
+#[test]
+fn test_solve_problem_case_3() {
+    let input: Vec<Item> = vec![
+        Item::NumberItem(" 51".to_string()),
+        Item::NumberItem("387".to_string()),
+        Item::NumberItem("215".to_string()),
+        Item::OperatorItem(Operator::Multiplication),
+    ];
+    let result = solve_problem(&input);
+    assert_eq!(result, 3253600);
+}
+
+#[test]
+fn test_solve_problem_case_4() {
+    let input: Vec<Item> = vec![
+        Item::NumberItem("64 ".to_string()),
+        Item::NumberItem("23 ".to_string()),
+        Item::NumberItem("314".to_string()),
+        Item::OperatorItem(Operator::Addition),
+    ];
+    let result = solve_problem(&input);
+    assert_eq!(result, 1058);
 }
